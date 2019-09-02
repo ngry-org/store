bd84c955ac190c9650b2b06f14c26c246963b700
achugaev <achugaev93@gmail.com>
2019-09-02 11:31:15 +0300

    initial implementation

---
+++ projects/store/src/lib/store.module.spec.ts
@@ -0,0 +1,248 @@
+import { map, tap } from 'rxjs/operators';
+import { RuntimeException } from '@monument/core';
+import { Action, Actions, Catch, Effect, EffectSource, Errors, Reaction, State, Store } from '@monument/store';
+import { Injectable } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+import { StoreModule } from './store.module';
+import Spy = jasmine.Spy;
+
+const LOAD = 'LOAD';
+const LOAD_SUCCESS = 'LOAD_SUCCESS';
+const LOAD_FAIL = 'LOAD_FAIL';
+
+interface Product {
+  name: string;
+  price: number;
+}
+
+class Load implements Action {
+  readonly type = LOAD;
+
+  constructor(readonly success: boolean) {}
+}
+
+class LoadSuccess implements Action {
+  readonly type = LOAD_SUCCESS;
+
+  constructor(readonly products: Product[]) {}
+}
+
+class LoadFail implements Action {
+  readonly type = LOAD_FAIL;
+
+  constructor(readonly error: CartException) {}
+}
+
+interface CartStateSnapshot {
+  readonly loading: boolean;
+  readonly loaded: boolean;
+  readonly products: Product[];
+  readonly totalPrice: number;
+  readonly error?: Error;
+}
+
+class CartException extends RuntimeException {
+}
+
+class CartState implements State<CartStateSnapshot> {
+  loading = false;
+  loaded = false;
+  products: Product[] = [];
+  error?: CartException;
+
+  get totalPrice() {
+    return this.products.reduce((price, product) => price + product.price, 0);
+  }
+
+  @Reaction(LOAD)
+  onLoad() {
+    this.loading = true;
+    this.loaded = false;
+    this.error = undefined;
+    this.products = [];
+  }
+
+  @Reaction(LOAD_SUCCESS)
+  onLoaded(action: LoadSuccess) {
+    this.loading = false;
+    this.loaded = true;
+    this.products = action.products;
+  }
+
+  @Reaction(LOAD_FAIL)
+  onLoadFail(action: LoadFail) {
+    this.loading = false;
+    this.loaded = true;
+    this.error = action.error;
+  }
+
+  getSnapshot(): CartStateSnapshot {
+    const {loading, loaded, error, products, totalPrice} = this;
+
+    return {
+      loading,
+      loaded,
+      products: [...products],
+      totalPrice,
+      error
+    };
+  }
+}
+
+@Injectable()
+class CartStore extends Store<CartStateSnapshot, CartState> {
+  constructor(actions: Actions) {
+    super(actions, new CartState());
+  }
+}
+
+@Injectable()
+class CartEffects {
+  @Effect()
+  readonly load: EffectSource = this.actions.ofType<Load>(LOAD).pipe(
+    map(action => {
+      if (action.success) {
+        return new LoadSuccess([
+          {
+            name: 'Product1',
+            price: 1
+          },
+          {
+            name: 'Product2',
+            price: 2
+          }
+        ]);
+      } else {
+        return new LoadFail(new CartException('Error occurred while loading cart'));
+      }
+    })
+  );
+
+  @Effect({dispatch: false})
+  readonly loadFail: EffectSource = this.actions.ofType<LoadFail>(LOAD_FAIL).pipe(
+    tap(action => {
+      this.errors.next(action.error);
+    })
+  );
+
+  constructor(readonly actions: Actions, readonly errors: Errors) {
+  }
+}
+
+@Injectable()
+class CartErrorHandlers {
+  @Catch(CartException)
+  add(exception: CartException) {
+    console.error(exception);
+  }
+}
+
+function testStoreModule(description: string, setup: Function) {
+  describe('StoreModule', () => {
+    describe(description, function () {
+      let actions: Actions;
+      let errors: Errors;
+      let cartStore: CartStore;
+      let actionsSpy: Spy;
+      let errorsSpy: Spy;
+      let consoleError: Spy;
+
+      beforeEach(() => {
+        setup();
+        actions = TestBed.get(Actions);
+        errors = TestBed.get(Errors);
+        cartStore = TestBed.get(CartStore);
+        actionsSpy = spyOn(actions, 'next').and.callThrough();
+        errorsSpy = spyOn(errors, 'next').and.callThrough();
+        consoleError = spyOn(console, 'error').and.stub();
+      });
+
+      it('should provide actions stream', () => {
+        expect(actions).toBeInstanceOf(Actions);
+      });
+
+      it('should provide errors stream', () => {
+        expect(errors).toBeInstanceOf(Errors);
+      });
+
+      it('should provide store instance', () => {
+        expect(cartStore).toBeInstanceOf(CartStore);
+      });
+
+      it('should invoke @Reaction and @Catch methods', () => {
+        expect(actionsSpy).toHaveBeenCalledTimes(0);
+        expect(errorsSpy).toHaveBeenCalledTimes(0);
+        expect(cartStore.snapshot).toEqual({
+          loading: false,
+          loaded: false,
+          products: [],
+          totalPrice: 0
+        });
+        actions.next(new Load(true));
+        expect(actionsSpy).toHaveBeenCalledTimes(2);
+        expect(actionsSpy).toHaveBeenNthCalledWith(1, new Load(true));
+        expect(actionsSpy).toHaveBeenNthCalledWith(2, new LoadSuccess([
+          {
+            name: 'Product1',
+            price: 1
+          },
+          {
+            name: 'Product2',
+            price: 2
+          }
+        ]));
+        expect(errorsSpy).toHaveBeenCalledTimes(0);
+        expect(cartStore.snapshot).toEqual({
+          loading: false,
+          loaded: true,
+          products: [
+            {
+              name: 'Product1',
+              price: 1
+            },
+            {
+              name: 'Product2',
+              price: 2
+            }
+          ],
+          totalPrice: 3
+        });
+        actions.next(new Load(false));
+        expect(cartStore.snapshot).toEqual({
+          loading: false,
+          loaded: true,
+          products: [],
+          totalPrice: 0,
+          error: new CartException('Error occurred while loading cart')
+        });
+        expect(consoleError).toHaveBeenNthCalledWith(1, new CartException('Error occurred while loading cart'));
+        expect(errorsSpy).toHaveBeenCalledTimes(1);
+      });
+    });
+  });
+}
+
+testStoreModule('forRoot()', () => {
+  TestBed.configureTestingModule({
+    imports: [
+      StoreModule.forRoot({
+        store: CartStore,
+        effects: [CartEffects],
+        errorHandlers: [CartErrorHandlers]
+      })
+    ]
+  });
+});
+
+testStoreModule('forFeature()', () => {
+  TestBed.configureTestingModule({
+    imports: [
+      StoreModule.forRoot(),
+      StoreModule.forFeature({
+        store: CartStore,
+        effects: [CartEffects],
+        errorHandlers: [CartErrorHandlers]
+      })
+    ]
+  });
+});
---
+++ projects/store/src/lib/store.module.ts
@@ -0,0 +1,106 @@
+import { Type } from '@monument/core';
+import { Actions, EffectMediator, ErrorMediator, Errors, Store } from '@monument/store';
+import { Inject, InjectionToken, ModuleWithProviders, NgModule, Optional } from '@angular/core';
+
+const STORE: InjectionToken<Store<any, any>> = new InjectionToken('STORE');
+const EFFECTS: InjectionToken<Array<object>> = new InjectionToken('EFFECTS');
+const ERROR_HANDLERS: InjectionToken<Array<object>> = new InjectionToken('ERROR_HANDLERS');
+
+export interface StoreModuleConfiguration {
+  readonly store?: Type<Store<any, any>>;
+  readonly effects?: Array<Type<object>>;
+  readonly errorHandlers?: Array<Type<object>>;
+}
+
+@NgModule()
+export class StoreModule {
+  static forRoot(configuration: StoreModuleConfiguration = {}): ModuleWithProviders {
+    return {
+      ngModule: StoreModule,
+      providers: [
+        {
+          provide: Actions,
+          useFactory() {
+            return new Actions();
+          }
+        },
+        {
+          provide: Errors,
+          useFactory() {
+            return new Errors();
+          }
+        },
+        ...(configuration.store ? [
+          {
+            provide: configuration.store,
+            useClass: configuration.store
+          },
+          {
+            provide: STORE,
+            useExisting: configuration.store
+          }
+        ] : []),
+        ...(configuration.effects || []).map(effectsClass => {
+          return {
+            provide: EFFECTS,
+            useClass: effectsClass,
+            multi: true
+          };
+        }),
+        ...(configuration.errorHandlers || []).map(errorHandlerClass => {
+          return {
+            provide: ERROR_HANDLERS,
+            useClass: errorHandlerClass,
+            multi: true
+          };
+        })
+      ]
+    };
+  }
+
+  static forFeature(configuration: StoreModuleConfiguration): ModuleWithProviders {
+    return {
+      ngModule: StoreModule,
+      providers: [
+        ...(configuration.store ? [
+          {
+            provide: configuration.store,
+            useClass: configuration.store
+          },
+          {
+            provide: STORE,
+            useExisting: configuration.store
+          }
+        ] : []),
+        ...(configuration.effects || []).map(effectsClass => {
+          return {
+            provide: EFFECTS,
+            useClass: effectsClass,
+            multi: true
+          };
+        }),
+        ...(configuration.errorHandlers || []).map(errorHandlerClass => {
+          return {
+            provide: ERROR_HANDLERS,
+            useClass: errorHandlerClass,
+            multi: true
+          };
+        })
+      ]
+    };
+  }
+
+  private effectMediator: EffectMediator;
+  private errorMediator: ErrorMediator;
+
+  constructor(
+    actions: Actions,
+    errors: Errors,
+    @Optional() @Inject(STORE) store: Store<any, any> | null,
+    @Optional() @Inject(EFFECTS) effects: Array<object> | null,
+    @Optional() @Inject(ERROR_HANDLERS) errorHandlers: Array<object> | null
+  ) {
+    this.effectMediator = new EffectMediator(actions, effects || []);
+    this.errorMediator = new ErrorMediator(errors, errorHandlers || []);
+  }
+}
